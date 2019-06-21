432f36b01a2d83737acea7bb120e5f9e266cac45
Falcon Rising <chinnidiwakar5@gmail.com>
2019-06-21 17:34:11 +0530

    Update arpsweep.sh

--- arpsweep.sh
+++ arpsweep.sh
@@ -2,5 +2,5 @@
 echo "Enter First Three Octets ex: 192.168.0. finish with a dot"
 read iprange
 for host in $(seq 1 254);
-do arping -c 1 iprange$host |grep "reply from" |cut -d " " -f4;
+do arping -c 1 $iprange$host |grep "reply from" |cut -d " " -f4;
 done;
